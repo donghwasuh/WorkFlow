a5444204e301e5e7ae024f8c9dc4ea0f3b0b2a5c
mysqli <dhdh0113@gmail.com>
2018-04-23 11:36:23 +0900

    env path 설정 수정

--- env/env.sh
+++ env/env.sh
@@ -1,11 +1,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export EVENTFLOW_HOME=`echo $DIR | sed 's/\/env//g'`
-LIB_CHECK=`ls -alh ${EVENTFLOW_HOME}/lib | grep EVENTFLOW_CORE | wc -l`
+export WORKFLOW_HOME=`echo $DIR | sed 's/\/env//g'`
+LIB_CHECK=`ls -alh ${WORKFLOW_HOME}/lib | grep WORKFLOW_CORE | wc -l`
 
 if [ "${LIB_CHECK}" -eq "0" ]
 then
-    export PYTHONPATH=${EVENTFLOW_HOME}/lib:${PYTHONPATH}
+    export PYTHONPATH=${WOKRFLOW_HOME}/lib:${PYTHONPATH}
 else
-    export PYTHONPATH=${EVENTFLOW_HOME}/lib/EVENTFLOW_CORE.zip:${PYTHONPATH}
+    export PYTHONPATH=${WORKFLOW_HOME}/lib/EVENTFLOW_CORE.zip:${PYTHONPATH}
 fi
-    
+   
